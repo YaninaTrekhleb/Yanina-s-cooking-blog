13120dd4501f8382d764fef097637a45f270438a
Yanina Trekhleb <yanisavchenko@gmail.com>
2018-03-08 20:07:50 +0000

    js form

---
+++ js/form.js
@@ -0,0 +1,30 @@
+$('#contact-form').submit(onContactFormSubmit);
+
+function onContactFormSubmit(event) {
+  event.preventDefault();
+  
+  const name = $('#contact-form input[name=name]').val();
+  const email = $('#contact-form input[name=email]').val();
+  const note = $('#contact-form input[name=note]').val();
+  const data = {
+      name: name, 
+      email: email,
+      note: note,
+  };
+  
+   $.ajax({
+        url: "https://tranquil-sierra-86540.herokuapp.com/yanisavchenko@gmail.com",
+        method: "POST",
+        data: data,
+        dataType: "json"
+    })
+    .done(function() {
+        $('#contact-form input[name=name]').val('');
+        $('#contact-form input[name=email]').val('');
+        $('#contact-form input[name=note]').val('');
+        alert('Message was sent');
+    })
+    .fail(function() {
+        alert('Error');
+    });
+}
